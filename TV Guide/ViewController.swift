fef01ee6b30acba5432ac948b174bb14d0238874
Palle Klewitz <palle.klewitz@tum.de>
2017-11-17 21:39:45 +0100

    Initial Commit

---
+++ TV Guide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TV Guide
+//
+//  Created by Palle Klewitz on 17.11.17.
+//  Copyright © 2017 Technische Universität München. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
