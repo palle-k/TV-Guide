86806b02b23da48fb37e9da31f96917dc5675fd0
Luca Klingenberg <luca.klingenberg@tum.de>
2017-11-18 03:15:07 +0100

    change textview to label in tableview

---
+++ TV Guide/ProgramInfoTableViewController.swift
@@ -0,0 +1,17 @@
+
+import UIKit
+
+class ProgramInfoTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.tableView.estimatedRowHeight = 80
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+    }
+}
