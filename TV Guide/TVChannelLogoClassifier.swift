f5b5b75b7860473019e9b7879e9329f845a4f72b
Palle Klewitz <palle.klewitz@tum.de>
2017-11-18 16:06:14 +0100

    Stuff for logo classification

--- TV Guide/TVChannelLogoClassifier.swift
+++ TV Guide/TVChannelLogoClassifier.swift
@@ -3,7 +3,7 @@
 //  TV Guide
 //
 //  Created by Palle Klewitz on 18.11.17.
-//  Copyright © 2017 Technische Universität München. All rights reserved.
+//  Copyright © 2017 Palle Klewitz. All rights reserved.
 //
 
 import Foundation
@@ -15,11 +15,41 @@ class TVChannelLogoClassifier {
 		
 	}
 	
-	func predictChannel(in image: CIImage) -> [String: Float] {
+	func predictTVChannelName(in image: CIImage) -> [String: Float] {
+		let logoAreas = extractPossibleLogoAreas(in: image)
 		return [:]
 	}
 	
 	private func extractPossibleLogoAreas(in image: CIImage) -> [CIImage] {
-		return []
+		
+		let cropAreaSizeFraction: CGFloat = 6
+		
+		let bounds = [
+			CGRect(
+				x: image.extent.minX,
+				y: image.extent.minY,
+				width: image.extent.width / cropAreaSizeFraction,
+				height: image.extent.height / cropAreaSizeFraction
+			),
+			CGRect(
+				x: image.extent.minX + image.extent.width * (cropAreaSizeFraction - 1) / cropAreaSizeFraction,
+				y: image.extent.minY,
+				width: image.extent.width / cropAreaSizeFraction,
+				height: image.extent.height / cropAreaSizeFraction
+			),
+			CGRect(
+				x: image.extent.minX,
+				y: image.extent.minY + image.extent.height * (cropAreaSizeFraction - 1) / cropAreaSizeFraction,
+				width: image.extent.width / cropAreaSizeFraction,
+				height: image.extent.height / cropAreaSizeFraction
+			),
+			CGRect(
+				x: image.extent.minX + image.extent.width * (cropAreaSizeFraction - 1) / cropAreaSizeFraction,
+				y: image.extent.minY + image.extent.height * (cropAreaSizeFraction - 1) / cropAreaSizeFraction,
+				width: image.extent.width / cropAreaSizeFraction,
+				height: image.extent.height / cropAreaSizeFraction
+			)
+		]
+		return bounds.map(image.cropped(to:))
 	}
 }
